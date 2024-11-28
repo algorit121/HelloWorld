7ffda64d07ff03bfe857fabc5bd6175ad6fde007
algorit121 <algorit121@gmail.com>
2024-11-28 23:52:13 +0300

    Rfi
    xx

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Водит консоль приветствующую надпись
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
